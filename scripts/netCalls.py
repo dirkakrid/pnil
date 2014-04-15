e4659fcb2803028f4c4c5f9add9e35b2c2ae740d
Yandy Ramirez <yandyr@gmail.com>
2014-04-15 01:46:17 -0400

    created key kargs in main to test function and argument passing

--- scripts/netCalls.py
+++ scripts/netCalls.py
@@ -99,10 +99,9 @@ def main():
         sw1.setLogin('arista', 'arista')
         # # function = 'getHostname, getVersion, getPlatform, getCPU, getDetails'
         function = 'getRoutes'
+        kargs = {'vrf': None, 'options': 'ospf'}
         result = sw1.run(function)
-        # result = sw1.run(function, {'vrf': None, 'options': 'ospf'})
-        # result = sw1.run(function, {'vrf': 'mgmt', 'options': None})
-        # result = sw1.run(function, {'vrf': None, 'options': None})
+        # result = sw1.run(function, kargs)
 
         printResult(result)
 
